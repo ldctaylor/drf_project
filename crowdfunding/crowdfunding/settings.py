2c6135878e3ce0358ee94850b568ae55c2b74489
Leah <ldctaylor@gmail.com>
2023-07-29 12:22:34 +1000

    Commit classwork

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
+    'projects.apps.ProjectsConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
