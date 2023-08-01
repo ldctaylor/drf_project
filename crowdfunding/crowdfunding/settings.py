c5c6120820b3fbf7e8b0f9c4f3037eba30fd6c6e
Leah <ldctaylor@gmail.com>
2023-08-01 18:35:20 +1000

    Apply custom user model and reapply migrations

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -31,15 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects.apps.ProjectsConfig',
-    'rest_framework'
+    'rest_framework',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
