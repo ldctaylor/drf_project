786612615a9f50ea69c39a040b8ad193fdd260dd
Leah <ldctaylor@gmail.com>
2023-08-01 19:44:51 +1000

    Add self referencing foreign key to users table, and views for custom users

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 class CustomUser(AbstractUser):
-    pass
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username
