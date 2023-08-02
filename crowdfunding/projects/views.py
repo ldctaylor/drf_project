2ba0fa1ad243e49482a6d1192ee49d8500671ac5
Leah <ldctaylor@gmail.com>
2023-08-02 10:45:12 +1000

    Add usccess and error codes for put request

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -50,7 +50,14 @@ class ProjectDetail(APIView):
         )
         if serializer.is_valid():
             serializer.save()
-            
+            return Response(
+                serializer.data,
+                status=status.HTTP_204_NO_CONTENT
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 class PledgeList(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
