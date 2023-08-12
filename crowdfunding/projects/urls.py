b09565d29bd9440d8da826fff6474a6a1a6cc0c2
Leah <ldctaylor@gmail.com>
2023-08-12 19:29:29 +1000

    Add endpoints for condition

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
     path('projects/<int:pk>/', views.ProjectDetail.as_view()),
     path('pledges/', views.PledgeList.as_view()),
-    path('pledge/<int:pk>/', views.PledgeDetail.as_view())
+    path('pledge/<int:pk>/', views.PledgeDetail.as_view()),
+    path('conditions/',views.ConditionList.as_view()),
+    path('condition/<int:pk>/',views.ConditionDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
